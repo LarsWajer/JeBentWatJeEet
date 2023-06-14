dbbf8cfc899c30c050ed65d5a931edcec316168a
Perry J.R. Rijaard <93517178+Ch5pper@users.noreply.github.com>
2023-06-14 11:52:48 +0200

    Nieuwe pagina's voor React js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,62 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
 import './App.css';
+import Home from './Home';
+import About from './About';
+import Contact from './Contact';
 
 function App() {
+  const [currentPage, setCurrentPage] = useState('home');
+  const [showTransition, setShowTransition] = useState(false);
+
+  const renderPage = () => {
+    switch (currentPage) {
+      case 'home':
+        return <Home />;
+      case 'about':
+        return <About />;
+      case 'contact':
+        return <Contact />;
+      default:
+        return <Home />;
+    }
+  };
+
+  useEffect(() => {
+    setShowTransition(true);
+    const timeout = setTimeout(() => {
+      setShowTransition(false);
+    }, 500);
+    return () => clearTimeout(timeout);
+  }, [currentPage]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {showTransition && <div className="page-transition" />}
+      <nav className="top-navbar">
+        <div className="logo-left">
+          <h1>profile</h1>
+        </div>
+        <div className="logo-middle">
+          <h1>JBWJE</h1>
+        </div>
+        <div className="logo-right">
+        <img src="/jbwjelogo.png" alt="logo" />
+        </div>
+      </nav>
+      {renderPage()}
+      <nav className="bottom-navbar">
+        <ul>
+          <li>
+            <button onClick={() => setCurrentPage('home')}>Home</button>
+          </li>
+          <li>
+            <button onClick={() => setCurrentPage('about')}>Recepten</button>
+          </li>
+          <li>
+            <button onClick={() => setCurrentPage('contact')}>Dagboek</button>
+          </li>
+        </ul>
+      </nav>
     </div>
   );
 }
---
+++ src/Home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Home() {
+    return (
+        <div class='container'>
+          <h1>Home Page</h1>
+          <p>Dit is de homepagina van jbwje</p>
+        </div>
+      );
+}
+
+export default Home;
