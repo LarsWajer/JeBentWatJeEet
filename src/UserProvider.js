965df111de586cee3107d4e4f5a93a22bbb059e0
Perry J.R. Rijaard <93517178+Ch5pper@users.noreply.github.com>
2023-06-20 00:18:12 +0200

    Update Userprovider.js

--- src/UserProvider.js
+++ src/UserProvider.js
@@ -1,18 +1,31 @@
-import React, { createContext, useState } from 'react';
+import React, { createContext, useState, useEffect } from 'react';
 
+// Create the UserContext
 export const UserContext = createContext();
 
+// Create the UserProvider component
 export const UserProvider = ({ children }) => {
   const [userData, setUserData] = useState(null);
 
   const login = (user) => {
     setUserData(user);
+    localStorage.setItem('userData', JSON.stringify(user));
   };
 
   const logout = () => {
     setUserData(null);
+    localStorage.removeItem('userData');
   };
 
+  // Retrieve user data from local storage on component mount
+  useEffect(() => {
+    const storedUserData = localStorage.getItem('userData');
+    if (storedUserData) {
+      setUserData(JSON.parse(storedUserData));
+    }
+  }, []);
+
+  // Provide the user data and login/logout functions to the children components
   return (
     <UserContext.Provider value={{ userData, login, logout }}>
       {children}
