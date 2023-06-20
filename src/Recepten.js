d91cc0ebcae0c1f1db78fce750d3e1e8fdbc3098
LarsWajer <116730754+LarsWajer@users.noreply.github.com>
2023-06-20 15:00:04 +0200

    cssEnIngredienten

--- src/Recepten.js
+++ src/Recepten.js
@@ -10,6 +10,9 @@ const Recepten = () => {
         const fetchedRecipes = data.map(recipe => ({
           name: recipe.name,
           category: recipe.categorie,
+          ingredienten: recipe.ingredienten.map(ingredient => ({
+            naam: ingredient.ingredient,
+            hoeveelheid: ingredient.hoeveelheid})),
           bereidingswijze: recipe.bereidingswijze,
           key: uuidv4(),
         }));
@@ -38,7 +41,23 @@ const Recepten = () => {
             <div className="categoryContainer">
               <h2 className="recipeCategory"> Categorie: {recipe.category}</h2>
             </div>
-            {isVisible && <div className="extraContent">{recipe.bereidingswijze}</div>}
+            {isVisible && (
+              <div className="extraContent">
+                <div className='ingredienten'>
+                <h3>Ingrediënten:</h3>
+                <ul className='list'>
+                  {recipe.ingredienten.map(ingredient => (
+                    <li  key={uuidv4()}>
+                      {ingredient.naam} - {ingredient.hoeveelheid}
+                    </li>
+                  ))}
+                </ul>
+                </div>
+                <h3>Bereidingswijze:</h3>
+                <p>{recipe.bereidingswijze}</p>
+              </div>
+            )}
+            
             <div className="buttonHolder">
               <button
                 onClick={handleButtonClick}
