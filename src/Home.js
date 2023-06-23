f047b9849b7b1fe3bf72b7fbdb2d15179072ece5
Perry J.R. Rijaard <93517178+Ch5pper@users.noreply.github.com>
2023-06-23 15:52:58 +0200

    Update

--- src/Home.js
+++ src/Home.js
@@ -1,32 +1,54 @@
-import React from 'react';
+import React, { useContext, useEffect, useState } from 'react';
+import { UserContext } from './UserContext';
 
 const Home = () => {
+  const { userData } = useContext(UserContext);
+  const [streak, setStreak] = useState(0);
+  const [showStreakCircle, setShowStreakCircle] = useState(false);
+
+  useEffect(() => {
+    // Fetch and update the streak from the user data or any other source
+    // Here, we are using a random value as an example
+    const randomStreak = Math.floor(Math.random() * 7) + 1;
+    setStreak(randomStreak);
+  }, []);
+
+  useEffect(() => {
+    if (userData) {
+      setShowStreakCircle(true);
+    }
+  }, [userData]);
+
+  const handleStreakPopup = () => {
+    if (streak === 7) {
+      alert('Congratulations! You have reached your goal for a healthy lifestyle!');
+    } else {
+      alert('Better next time, buddy!');
+      setStreak(0);
+    }
+  };
+
   return (
     <div className='container'>
-    <div className="landing-page">
-      <header className="header">
-        <h1>Welkom !</h1>
-        <p>Dit is de hoofdpagina van Je Bent Wat Je Eet App</p>
-      </header>
+      <div className="landing-page">
+        <div className="widget">
+          <h1>Welkom! {userData && userData.name}</h1>
 
-      <section className="features">
-        <div className="feature">
-          <img src="feature-1.png" alt="Feature 1" />
-          <h2>Feature 1</h2>
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-          <button className="cta-button">Sign Up</button>
+          <section className="features">
+            <div className="feature">
+              <h2>Feature 1</h2>
+              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+              <button className="cta-button" onClick={handleStreakPopup}>Sign Up</button>
+            </div>
 
+            {showStreakCircle && (
+              <div className="streak-circle">
+                <p>Streak: {streak} days</p>
+              </div>
+            )}
+          </section>
         </div>
-
-        <div className="feature">
-          <img src="feature-2.png" alt="Feature 2" />
-          <h2>Feature 2</h2>
-          <p>Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris.</p>
-          <button className="cta-button">Sign Up</button>
-        </div>
-      </section>
-
-    </div>
+      </div>
     </div>
   );
 };
